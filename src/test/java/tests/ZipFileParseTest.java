ecd6443e2df801d57c17d6b1d27affed379f94b9
kostenko.dv <dmitriy.kostenko@sbermarket.ru>
2025-05-19 21:21:28 +0300

    refactored zip archive test

--- src/test/java/tests/ZipFileParseTest.java
+++ src/test/java/tests/ZipFileParseTest.java
@@ -3,48 +3,75 @@ package tests;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
-import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class ZipFileParseTest {
 
     ClassLoader cl = ZipFileParseTest.class.getClassLoader();
 
     @Test
-    void zipTest() throws Exception {
-        ZipFile zf = new ZipFile(new File("src/test/resources/sample-zip-file.zip"));
-        ZipInputStream zis = new ZipInputStream(cl.getResourceAsStream("sample-zip-file.zip"));
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-            try (InputStream inputStream = zf.getInputStream(entry)) {
-                String entryName = entry.getName();
-                if (entryName.contains(".pdf")) {
-                    PDF pdf = new PDF(inputStream);
-                    assertThat(pdf.text).contains("Lorem ipsum");
-                } else if (entryName.contains(".csv")) {
-                    try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
+    @DisplayName("Проверка наличия и контента для PDF файла")
+    public void parsePDFFileInZipArchiveTest() throws Exception {
+        try (InputStream inputStream = cl.getResourceAsStream("sample-zip-file.zip")) {
+            assert inputStream != null;
+            try (ZipInputStream zis = new ZipInputStream(inputStream)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().contains(".pdf")) {
+                        PDF pdf = new PDF(zis);
+                        assertThat(pdf.text).contains("Lorem ipsum");
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка наличия и контента для CSV файла")
+    public void parseCSVFileInZipArchiveTest() throws Exception {
+        try (InputStream inputStream = cl.getResourceAsStream("sample-zip-file.zip")) {
+            assert inputStream != null;
+            try (ZipInputStream zis = new ZipInputStream(inputStream)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().contains(".csv")) {
+                        CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
                         List<String[]> content = csvReader.readAll();
                         String[] row = content.get(1);
                         assertThat(row[1]).isEqualTo("Dulce");
                         assertThat(row[3]).isEqualTo("Female");
                     }
-                } else if (entryName.contains(".xls")) {
-                    XLS xls = new XLS(inputStream);
-                    assertThat(
-                            xls.excel.getSheetAt(0)
-                                    .getRow(2)
-                                    .getCell(2)
-                                    .getStringCellValue()
-                    ).isEqualTo("Hashimoto");
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка наличия и контента для XLS файла")
+    public void parseXLSFileInZipArchiveTest() throws Exception {
+        try (InputStream inputStream = cl.getResourceAsStream("sample-zip-file.zip")) {
+            assert inputStream != null;
+            try (ZipInputStream zis = new ZipInputStream(inputStream)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().contains(".xls")) {
+                        XLS xls = new XLS(zis);
+                        assertThat(
+                                xls.excel.getSheetAt(0)
+                                        .getRow(2)
+                                        .getCell(2)
+                                        .getStringCellValue()
+                        ).isEqualTo("Hashimoto");
+                    }
                 }
             }
         }
